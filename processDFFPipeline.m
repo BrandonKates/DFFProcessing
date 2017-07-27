53f19148e07f4589ed115b703c0292feca4c2487
Brandon Kates <bjk224@cornell.edu>
2017-07-27 13:03:53 -0400

    Some updates to DFFProcessing.

--- processDFFPipeline.m
+++ processDFFPipeline.m
@@ -5,58 +5,98 @@ close all;
 % Json files are assumed to have centroid field, and nothing else is
 % assumed
 
+%% Find the json file
 [foldername, dir_nm] = uigetfile('.json');
 fullpath = fullfile(dir_nm,foldername); % Set the file name using this variable
-one_long_file = true;
-if one_long_file
-    newNeurons = struct('nid',[],'dff',[]);
-    neurons = jsonread(fullpath);
-    for i = 1:length(neurons.jmesh)
-        newNeurons(i).nid = i;
-        newNeurons(i).dff = neurons.jmesh(i).dff;
-    end
-    
-    pullTimes = horzcat([1144 1175],[1515 1545],[1700 1735],[2445 2475],[3035 3065],[3465 3495]);% One input 
-    
-    pTA = 100; % frames before and after pull that should be included in the average
-    xpoints = (1:length([newNeurons.dff]));
-    
-    seconds = true;
-    framerate = 1;
-    if seconds
-        framerate = 30.31;
-    end
-    
-    dff=[newNeurons.dff];
-    pulls= struct('pullNum',[],'pullFrames',[],'average',[]);
-    pullNum = 1;
-    for i = 1:2:length(pullTimes)
-        thisPull = dff(pullTimes(i) - pTA : pullTimes(i+1) + pTA,:);
-        meanPull = mean(thisPull,2);
-        pulls(pullNum).pullNum = pullNum;
-        pulls(pullNum).pullFrames = [pullTimes(i) pullTimes(i+1)];
-        pulls(pullNum).average = meanPull;
-        pullNum = pullNum + 1;
-    end
-    
-    % Plot All Neurons
-    % plot(xpoints/framerate,[newNeurons.dff],'color',[0,0,0]+0.8)
-    plot(xpoints/framerate, [newNeurons([5 6 10 12 14]).dff]) % Active Neurons in this file
-    hold on;
-    plot(xpoints/framerate, mean([newNeurons([5 6 10 12 14]).dff],2),'g')
-    hold on;
-    % plot the pullFrames as vertical bars on the graph
-    for i = 1:length(pullTimes)
-        plot(repmat(pullTimes(i),1,2)/framerate,[0 2],'b')     
-    end
-    %plot the averages of the pullframes
-    for i = 1:length(pulls)
-        pull = pulls(i).pullFrames;
-        plot((pull(1)-pTA : pull(2)+pTA)/framerate, pulls(i).average,'r')
-        hold on;
-    end
-    xlabel('Time (s)')
+
+%% Load in variables for dff extraction from same directory.
+
+if(exist(fullfile(dir_nm, 'Fdf.mat'),'file'))
+    load(fullfile(dir_nm, 'Fdf.mat'))
 end
+if(exist(fullfile(dir_nm, 'Cd.mat'),'file'))
+    load(fullfile(dir_nm, 'Cd.mat'))
+end
+if(exist(fullfile(dir_nm, 'Sp.mat'),'file'))
+    load(fullfile(dir_nm, 'Sp.mat'))
+end
+
+
+%% Create a new struct with neuron data (nid, dff, Cd, and Sp) concatenated by time
+Fdf_concat = [];
+Sp_concat = [];
+Cd_concat = [];
+for i = 1:length(F_df)
+    if(exist('F_df','var'))
+        Fdf_concat = horzcat(Fdf_concat, cell2mat(F_df(i)));
+    end
+    if(exist('Sp','var'))
+        Sp_concat = horzcat(Sp_concat, cell2mat(Sp(i)));
+    end
+    if(exist('Cd','var'))
+        Cd_concat = horzcat(Cd_concat, cell2mat(Cd(i)));
+    end
+end
+
+newNeurons = struct('nid',[],'dff',[],'Cd',[],'Sp',[]);
+neurons = jsonread(fullpath);
+for i = 1:length(neurons.jmesh)
+    newNeurons(i).nid = i;
+    newNeurons(i).dff = Fdf_concat(i,:);
+    newNeurons(i).Sp = Sp_concat(i,:);
+    newNeurons(i).Cd = Cd_concat(i,:);
+end
+
+%% Initialize variables
+pullTimes = horzcat([1144 1175],[1515 1545],[1700 1735],[2445 2475],[3035 3065],[3465 3495]);% One input 
+
+pTA = 100; % frames before and after pull that should be included in the average
+xpoints = (1:length([newNeurons.dff]));
+
+seconds = true;
+framerate = 1;
+if seconds
+    framerate = 30.31;
+end
+
+
+badIndices = vertcat(find(vertcat(newNeurons.dff) > 4), find(vertcat(newNeurons.dff) < -1));
+dff = vertcat(newNeurons.dff);
+Cd = vertcat(newNeurons.Cd);
+Sp = vertcat(newNeurons.Sp);
+dff(badIndices) = 0;
+
+
+dff=[newNeurons.dff];
+pulls= struct('pullNum',[],'pullFrames',[],'average',[]);
+pullNum = 1;
+for i = 1:2:length(pullTimes)
+    thisPull = dff(pullTimes(i) - pTA : pullTimes(i+1) + pTA,:);
+    meanPull = mean(thisPull,2);
+    pulls(pullNum).pullNum = pullNum;
+    pulls(pullNum).pullFrames = [pullTimes(i) pullTimes(i+1)];
+    pulls(pullNum).average = meanPull;
+    pullNum = pullNum + 1;
+end
+
+% Plot All Neurons
+% plot(xpoints/framerate,[newNeurons.dff],'color',[0,0,0]+0.8)
+plot(xpoints/framerate, [newNeurons([5 6 10 12 14]).dff]) % Active Neurons in this file
+hold on;
+plot(xpoints/framerate, mean([newNeurons([5 6 10 12 14]).dff],2),'g')
+hold on;
+% plot the pullFrames as vertical bars on the graph
+for i = 1:length(pullTimes)
+    plot(repmat(pullTimes(i),1,2)/framerate,[0 2],'b')     
+end
+%plot the averages of the pullframes
+for i = 1:length(pulls)
+    pull = pulls(i).pullFrames;
+    plot((pull(1)-pTA : pull(2)+pTA)/framerate, pulls(i).average,'r')
+    hold on;
+end
+xlabel('Time (s)')
+
 
 active = [5 6 10 12 14];
 quiesc = [1 2 3 4 11 16];
